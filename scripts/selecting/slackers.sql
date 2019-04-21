6af2c2ae6e0b9d3b7807baa776b4c9626c1f971d
mavroian <mavflorin@gmail.com>
2019-04-21 18:32:11 +0900

    I think it works

--- scripts/selecting/slackers.sql
+++ scripts/selecting/slackers.sql
@@ -1,1 +1,8 @@
--- Your SQL here
+SELECT first_name, last_name 
+FROM students
+WHERE NOT EXISTS(
+  SELECT first_name,last_name, projects.id AS project_id
+FROM students_to_projects
+INNER JOIN students ON students.id = student_id 
+INNER JOIN projects ON projects.id = project_id
+WHERE  students.id != student_id);
