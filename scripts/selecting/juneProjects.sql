32bad770cefcab0e8682d11704284c5bb72da1e3
mavroian <mavflorin@gmail.com>
2019-04-21 18:31:38 +0900

    juneProjects Complete

--- scripts/selecting/juneProjects.sql
+++ scripts/selecting/juneProjects.sql
@@ -1,1 +1,5 @@
--- Your SQL here
+SELECT name, checkins.id,students.first_name
+FROM projects
+INNER JOIN students on students.id =  projects.id
+INNER JOIN checkins on checkins.id = students.id
+WHERE  EXTRACT(month from checked_in_at) = 6;
