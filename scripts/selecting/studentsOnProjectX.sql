b0e53182f4fee75ad5f5c78223332f0649e4f686
mavroian <mavflorin@gmail.com>
2019-04-21 18:32:32 +0900

    StudentsOnProjectX Complete

--- scripts/selecting/studentsOnProjectX.sql
+++ scripts/selecting/studentsOnProjectX.sql
@@ -1,1 +1,6 @@
--- Your SQL here
+SELECT students.id,first_name,last_name, projects.name,projects.id AS project_id
+FROM students_to_projects
+INNER JOIN students ON students.id = student_id 
+INNER JOIN projects ON projects.id = project_id
+ORDER BY 
+  student_id ASC;
