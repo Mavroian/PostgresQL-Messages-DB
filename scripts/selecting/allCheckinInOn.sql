dfd1810a44435a92fa9728f0b155f6dc496d40f7
mavroian <mavflorin@gmail.com>
2019-04-21 18:30:57 +0900

    allCheckinInOut Complete

--- scripts/selecting/allCheckinInOn.sql
+++ scripts/selecting/allCheckinInOn.sql
@@ -1,1 +1,5 @@
---  your SQL here
+SELECT checked_in_at, students.id, first_name, last_name, date_of_birth, gender, town_of_birth
+from students
+inner join checkins                                                   
+ on students.id = checkins.student_id
+ where EXTRACT(day from checked_in_at)>=1 and extract(day from checked_in_at)<=31 ORDER BY checked_in_at ASC;
